dbbfbedbe1cfe6b87957dcd9d092f77004072018
p312734 <34969190+p312734@users.noreply.github.com>
2024-11-15 23:55:46 +0100

    in MISC.PY: TRANSCRIPT normalized

--- misc.py
+++ misc.py
@@ -1,15 +1,41 @@
 import pandas as pd
+import ast
+import re
 
-df = pd.DataFrame({
-    'A': [1, 2, 3],
-    'B': [4, 5, 6]
-})
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', 100)
 
-print(df)
+transcript = pd.read_json('data/transcript.json', orient='records', lines=True)
 
-col = 'A'
+def split_dict_into_cols(df, col):
+    ''' 
+    For an entire Pandas dataframe normalize the dictionary value pairs from
+    column col into individual columns named by key value.
+    
+    Arguments:
+    df - Pandas dataframe
+    col - Column name containing dictionary values as strings
+    
+    Returns:
+    df_result - Dataframe with multivalue column normalized into separate columns
+    df_conversion_errors - Dataframe with rows that failed to convert
+    '''   
 
-for index, row in df.iterrows():
-    if row[col]:
-        print(f"Index: {index}, Row: {row['A'], row['B']}, {row[col]}")
-        
+    # Initialize a list to store rows that fail to convert
+    conversion_errors = []
+
+    # Iterate over each row in the dataframe
+    for index, row in df.iterrows():
+
+        dictval = row[col]
+        for key, value in dictval.items():
+            if key == 'offer id':
+                key = 'offer_id'
+            # print(f"Key: {key}, Value: {value}")
+            #df.loc[index, key] = value
+            df.loc[index, (key)] = value
+      
+    return df 
+
+df_result = split_dict_into_cols(transcript, 'value')
+df_result.to_excel('data/transcript_normalized.xlsx')
